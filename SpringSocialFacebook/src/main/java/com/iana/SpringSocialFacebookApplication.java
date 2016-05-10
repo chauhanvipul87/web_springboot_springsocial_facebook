c9d8249bfedb133eb5554f3fd1e3f86681ac31e7
vipul Chauhan <test.jpatel1@gmail.com>
2016-05-10 13:57:38 +0530

    initial commit

---
+++ SpringSocialFacebook/src/main/java/com/iana/SpringSocialFacebookApplication.java
@@ -0,0 +1,12 @@
+package com.iana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocialFacebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSocialFacebookApplication.class, args);
+	}
+}
